19fb072210b30f0597f9a85361ef4b822d9503fe
RoxyRomanyshyn <iamroksa@icloud.com>
2020-03-19 22:22:53 +0200

    add hw3

---
+++ src2/script.js
@@ -0,0 +1,21 @@
+const user = {
+    name: 'Mango',
+    age: 20,
+    hobby: 'html',
+    premium: true,
+};
+
+user.mood = 'happy';
+console.log(user.mood);
+
+user.hobby = 'skydiving';
+console.log(user.hobby);
+
+user.premium = 'false';
+console.log(user.premium);
+
+const keys = Object.keys(user);
+
+for (const key of keys) {
+    console.log('key: ', user[key]);
+}
