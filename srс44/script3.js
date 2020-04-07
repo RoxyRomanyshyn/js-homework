e66b154d96e77abfd8995a2f802e3269563dfc52
RoxyRomanyshyn <iamroksa@icloud.com>
2020-04-07 21:33:04 +0300

    fixed

--- srс44/script3.js
+++ srс44/script3.js
@@ -1,28 +1,26 @@
 class StringBuilder {
-    constructor(value) {
-        this._value = value;
-    }
-    get value() {
-        return this._value;
-    }
-    append() {
-        return this._value;
-    }
-    prepend() {
-        return this._value;
-    }
-    pad() {
-        return this._value;
-    }
+  constructor(value) {
+    this._value = value;
+  }
+  get value() {
+    return this._value;
+  }
+  append(str) {
+    this._value = this._value + str;
+  }
+  prepend(strn) {
+    this._value = strn + this._value;
+  }
+  pad(strng) {
+    this._value = strng + this._value + strng;
+  }
 }
-
 
 // Метод append(str) - получает парметр str (строку) и добавляет ее в конец _value
 // Метод prepend() - получает парметр str(строку) и добавляет ее в начало value
 // Метод pad(str) - получает парметр str(строку) и добавляет ее в начало и в конец _value
 
 const builder = new StringBuilder('.');
-
 
 builder.append('^');
 console.log(builder.value); // '.^'
