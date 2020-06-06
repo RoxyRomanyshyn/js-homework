68eabf099cf765cef9a9773854d8d5b7393210b5
RoxyRomanyshyn <iamroksa@icloud.com>
2020-06-06 15:29:08 +0300

    add hw8

---
+++ src8/gallery.js
@@ -0,0 +1,54 @@
+import images from './gallery-items.js';
+
+const gallery = document.querySelector('.js-gallery');
+const model = document.querySelector('.js-lightbox');
+const image = document.querySelector('.lightbox__image');
+const button = document.querySelector('.lightbox__button');
+
+
+function addGall() {
+  const initHTML = images.reduce((acc, el) => {
+    acc += `<li class="gallery__item">
+        <a
+          class="gallery__link"
+          href="${el.original}"
+        >
+          <img
+            class="gallery__image"
+            src="${el.preview}"
+            data-source="${el.original}"
+            alt="${el.description}"
+          />
+        </a>
+      </li>`;
+    return acc;
+  }, '');
+
+  gallery.insertAdjacentHTML('beforeend', initHTML);
+}
+addGall();
+
+gallery.addEventListener('click', onClick);
+
+function onClick(e) {
+  e.preventDefault();
+  model.classList.add('is-open');
+  image.src = e.target.dataset.source;
+}
+button.addEventListener('click', closeClick);
+
+function closeClick(e) {
+  if (e && e.target === image) {
+    return;
+  }
+  model.classList.remove('is-open');
+  image.src = '';
+}
+
+document.addEventListener('keydown', esc);
+
+function esc(e) {
+  if (e.keyCode === 27) {
+    closeClick();
+  }
+}
